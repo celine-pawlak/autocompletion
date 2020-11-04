b49702b8ae8b0803388d6bf48254fd203a39057e
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-11-04 15:22:59 +0100

    début gestion d'erreur

--- API/indexAPI.php
+++ API/indexAPI.php
@@ -6,10 +6,38 @@ $pdo = $db->getPDO();
 if(isset($_GET['param']) && $_GET['param']=='getelement')
     {
         $id = $_GET['id'];
-        $req = $pdo->prepare('SELECT * FROM jeux WHERE id=?');
-        $req->execute([$id]);
-        $element = $req->fetch();
+        $error = [];
+        $set = [];
+        $requete = $pdo->prepare('SELECT id FROM jeux WHERE id=?');
+        $requete->execute([$id]);
+        $isInBdd = $requete->fetch();        
 
-        $json = json_encode($element);        
-        echo $json;
+        if(empty($isInBdd))
+            {
+                array_push($error, 'Pas la REF');               
+            }
+        if(!is_int($id))
+            {
+                array_push($error, 'La valeur n\'est pas correct');
+            }
+        if(empty($error))
+            {                            
+                $req = $pdo->prepare('SELECT * FROM jeux WHERE id=?');
+                $req->execute([$id]);
+                $element = $req->fetch();
+                
+                array_push($set, $element);
+                array_push($set, true);
+
+                $json = json_encode($set);        
+                echo $json;
+            }
+        else
+            {
+                array_push($error, false);
+                $json = json_encode($error);
+                echo $json;
+            }
+        
+        
     }
