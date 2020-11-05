f05a54e4fc0351a1f30f9567ee53bc13fa4e7652
Céline Pawlak <celine.pawlak@laplateforme.io>
2020-11-05 15:37:04 +0100

    fix boucle

--- API/indexAPI.php
+++ API/indexAPI.php
@@ -13,19 +13,20 @@ function nestedLowercase($value)
     }
 
 if (isset($_GET['param']) and $_GET['param'] == 'searchbar') {
-}
-if (!isset($allgames)) {
-    $allgames = $pdo->query("SELECT * FROM test")->fetchAll(PDO::FETCH_ASSOC);
-    $search = strtolower($_POST['search']);
-    $matches = [];
-    $allgameslower = array_map('nestedLowercase', $allgames);
-    for ($i = 0; $i < count($allgames); $i++) {
-        if (!empty($search) && preg_match('/' . $search . '/', $allgameslower[$i]['name'])) {
-            $matches[] = $allgames[$i];
+    if (!isset($allgames)) {
+        $allgames = $pdo->query("SELECT * FROM test")->fetchAll(PDO::FETCH_ASSOC);
+        $search = strtolower($_POST['search']);
+        $matches = [];
+        $allgameslower = array_map('nestedLowercase', $allgames);
+        for ($i = 0; $i < count($allgames); $i++) {
+            if (!empty($search) && preg_match('/' . $search . '/', $allgameslower[$i]['name'])) {
+                $matches[] = $allgames[$i];
+            }
         }
+        echo json_encode($matches);
     }
-    echo json_encode($matches);
 }
+
 // Gère l'envoie des données de l'élément séléctionner
 if(isset($_GET['param']) && $_GET['param']=='getelement')
     {      
